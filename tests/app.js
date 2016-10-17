024647a4350a3fb0b1518e4dda1f7b1a30f21ae6
A mobile-first, OOCSS framework <harmse@morainevalley.edu>
2016-10-17 11:31:01 -0500

    Updated

---
+++ tests/app.js
@@ -0,0 +1,11 @@
+/**
+ * Include your own tests below.
+ */
+describe('App', function() {
+	/**
+	 * Router
+	 */
+	it('Router', function() {
+		expect(leaf.isDefined(app.Router)).toBe(true);
+	});
+});
